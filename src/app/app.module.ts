f384d0f09083f18eb9dc0e35485c4e6f9dd9f59d
Abhishek Agrawal <abhishekacr@gmail.com>
2024-10-03 23:57:19 +0530

    seprate post comp code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { QuillModule } from 'ngx-quill';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,6 +16,9 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
 import { ToastrModule} from 'ngx-toastr';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AllPostComponent } from './posts/all-post/all-post.component';
+import { NewPostComponent } from './posts/new-post/new-post.component';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -22,20 +26,24 @@ import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
     HeaderComponent,
     FooterComponent,
     DashboardComponent,
-    CategoriesComponent
+    CategoriesComponent,
+    AllPostComponent,
+    NewPostComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule, ReactiveFormsModule,
     ToastrModule.forRoot(),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    QuillModule.forRoot()
   ],
   providers: [
     provideFirebaseApp(() => initializeApp({ "projectId": "infi-blog", "appId": "1:490064474978:web:4e50c3377db890a156723b", "storageBucket": "infi-blog.appspot.com", "apiKey": "AIzaSyDZcUCBIcLN6If3tv52VFzOojj3lrVDRgs", "authDomain": "infi-blog.firebaseapp.com", "messagingSenderId": "490064474978" })),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
-    provideStorage(() => getStorage())
+    provideStorage(() => getStorage()),
+    provideHttpClient(withFetch())
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/posts/new-post/new-post.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; 
+import { CategoriesService } from '../../services/categories.service';
+
+
+@Component({
+  selector: 'app-new-post',
+  templateUrl: './new-post.component.html',
+  styleUrl: './new-post.component.css'
+})
+export class NewPostComponent {
+
+  permalink: string = '';
+  imgSrc: any = '/assets/placeholder.png';
+  selectedImg:any;
+  categories: Array<any>;
+  constructor(private categoryService: CategoriesService){}
+
+  editorModules = {
+    toolbar: [
+      ['bold', 'italic', 'underline'],        // Toggle buttons
+      [{ 'header': 1 }, { 'header': 2 }],     // Custom dropdown
+      [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+      ['link', 'image']                       // Link and image insertion
+    ]
+  };
+  eplaceholder: string = "content";
+  ngOnInit(){
+    this.categoryService.loadData().subscribe( val => {
+      this.categories = val;
+    })
+  }
+
+  onTitleChange($event){
+    let title = $event.target.value;
+    this.permalink = title.replace(/\s/g,'-');
+  }
+
+  showPreview($event: any){
+    // read image through upload
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      this.imgSrc = e.target?.result
+    }
+    reader.readAsDataURL($event.target.files[0]);
+    this.selectedImg = $event.target.files[0];
+  }
+}
